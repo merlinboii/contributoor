a2a38972a9aa24a70a3a3a2c04ba43b657aad880
Filmptz <parichaya35@gmail.com>
2024-11-26 23:42:05 +0700

    feat: add available views

--- frontend/src/views/index.tsx
+++ frontend/src/views/index.tsx
@@ -1,6 +1,10 @@
 export { HomeView } from "./home";
 export { BasicsView } from "./basics";
-export { ContributorRegistrationView } from "./contributor-registration";
+
 export { TasksDashboardView } from "./tasks-dashboard";
-export { ProjectRegisterView } from "./project-registration";
+export { UserTasksDashboardView } from "./tasks-dashboard/user-tasks-dashboard";
 export { ProjectDashboardView } from "./project-dashboard";
+
+export { ContributorRegistrationView } from "./contributor-registration";
+export { ProjectRegisterView } from "./project-registration";
+export { CreateTaskView } from "./tasks";
